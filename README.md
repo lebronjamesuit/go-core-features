a01d9002929a424d3f606f26470b312159008a10
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-11-21 09:35:35 +0000

    Compile go main.go

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-core-features
+
+go run golang.org/x/telemetry/cmd/gotelemetry@latest on
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const name, age = "James", 33
+
+	fmt.Printf("%s oi  %d", name, age)
+}
